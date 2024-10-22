ffd5cf68e6d23f5f21680e544c973824e50b72e0
emi <abc@MacBook-Air.local>
2024-10-22 11:23:19 +0200

    Enhance Astro config with sitemap integration and update package dependencies accordingly; improve page metadata for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from "astro/config"
-
 import tailwind from "@astrojs/tailwind"
-
 import react from "@astrojs/react"
-
 import icon from "astro-icon"
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react(), icon()],
+	integrations: [tailwind(), react(), icon(), sitemap()],
 	site: "https://emanueldonner.github.io",
 	base: "toolbox24",
 })
