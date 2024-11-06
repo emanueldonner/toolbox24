272f4c8dcc20d1dc85558883a9e53b7a7e45e864
emi <abc@MacBook-Air.local>
2024-11-06 16:10:11 +0100

    Updated header bg color, top section bg color and added sustainability icon.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"primary-color": "var(--primary-color)",
+				"secondary-color": "var(--secondary-color)",
+				"secondary-bg-color": "var(--secondary-bg-color)",
+				"cta-color": "var(--cta-color)",
+			},
+		},
 	},
 	plugins: [],
 }
